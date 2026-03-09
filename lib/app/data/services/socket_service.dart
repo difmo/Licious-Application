1e6ad7fb5ab538000621de38a38feab210e114c7
Difmo <difmo@Difmos-Mac-mini.local>
2026-03-09 20:09:10 +0530

    save

--- lib/app/data/services/socket_service.dart
+++ lib/app/data/services/socket_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import '../network/api_client.dart';
@@ -10,33 +11,50 @@ class SocketService {
   }
 
   void _initSocket() {
+    _log('Initializing Socket: ${ApiClient.baseUrl}');
     socket = IO.io(ApiClient.baseUrl, <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': true,
     });
 
     socket.onConnect((_) {
-      print('Socket connected');
+      _log('CONNECTED', status: 'SUCCESS');
     });
 
     socket.onDisconnect((_) {
-      print('Socket disconnected');
+      _log('DISCONNECTED', status: 'WARNING');
     });
 
-    socket.onConnectError((err) => print('Socket connection error: $err'));
-    socket.onError((err) => print('Socket error: $err'));
+    socket.onConnectError(
+        (err) => _log('CONNECTION ERROR', data: err, status: 'ERROR'));
+    socket.onError((err) => _log('SOCKET ERROR', data: err, status: 'ERROR'));
+  }
+
+  void _log(String title, {dynamic data, String? status}) {
+    debugPrint('');
+    debugPrint(
+        '┌─── SOCKET ${status != null ? '[$status] ' : ''}────────────────────────────');
+    debugPrint('│ $title');
+    if (data != null) debugPrint('│ Data: $data');
+    debugPrint('└────────────────────────────────────────────');
   }
 
   void joinOrderRoom(String orderId) {
+    _log('JOIN ROOM', data: 'order_$orderId');
     socket.emit('join', 'order_$orderId');
   }
 
   void leaveOrderRoom(String orderId) {
+    _log('LEAVE ROOM', data: 'order_$orderId');
     socket.emit('leave', 'order_$orderId');
   }
 
   void onOrderUpdate(Function(dynamic) callback) {
-    socket.on('orderUpdate', callback);
+    _log('REGISTER CALLBACK', data: 'orderUpdate');
+    socket.on('orderUpdate', (data) {
+      _log('EVENT: orderUpdate', data: data);
+      callback(data);
+    });
   }
 
   void dispose() {
